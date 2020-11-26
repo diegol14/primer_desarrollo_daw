08189bce4678bc4542b6e5fbeecb48d165175aa3
Diegol14 <diegolpereira1414@gmail.com>
2020-11-26 20:45:11 +0100

    Delete usuarios en funciones

--- Funciones_db/funciones_usuarios.php
+++ Funciones_db/funciones_usuarios.php
@@ -13,6 +13,14 @@ function modifyUsuario() {
 
 function deleteUsuario($idUsuario) {
     echo "Borro usuario con Id:".$idUsuario ;
-    $myQueryDel="DELETE FROM usuarios where Id='$idUsuario'";
+    $queryDel="DELETE FROM usuarios where Id='$idUsuario'";
+    
+    if ($result = mysqli_query($connm07, $queryDel)) {
+        $affected_rows = mysqli_affected_rows($connm07);
+        if ($affected_rows==0) {
+            echo "<br/>No se han podido eliminar usuarios" ;
+        }
+        else{  echo "<br/>Registro/s borrado/s : ".$affected_rows; }
+    }
     
 }
